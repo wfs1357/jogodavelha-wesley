21d7004945788c8760d8ef0d29e4a787dfa783ba
wesley <106327635+wfs1357@users.noreply.github.com>
2023-11-24 09:34:09 -0300

    g

--- jogo.js
+++ jogo.js
@@ -9,7 +9,7 @@ const JOGADOR_O = "O"
 
 let jogoAcabou = false;
 
-document.addEventListener ("click", (event) =>) {
+document.addEventListener ("click", (event) => {
  if (event.target.matches(".celula") && !jogoAcabou) {
     jogar(event.target.id);
  }
